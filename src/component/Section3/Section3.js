1bfba0b474b5f137e0573d322fbb2980e23cc00d
kosy0907 <syko1234@naver.com>
2023-09-12 00:57:21 +0900

    디자인 수정

--- src/component/Section3/Section3.js
+++ src/component/Section3/Section3.js
@@ -44,7 +44,7 @@ function Section3(props, projectRef) {
                 {projectItem.map((item) => {
                     return (
                         <div className='projectSlide'>
-                            <img src={require(`../../images/mockup/${item.img}.png`)} alt='mockup' style={{ width: '150px' }} />
+                            <img src={require(`../../images/mockup/${item.img}.png`)} alt='mockup' style={{ width: '100px' }} />
                             <h1>{item.title}</h1>
                             <p>{item.description}</p>
                             <p>{item.link}</p>
