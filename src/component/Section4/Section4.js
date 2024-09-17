1a35902f25cb075882008eb646ed95ed34867432
kosy0907 <syko1234@naver.com>
2024-09-18 02:24:42 +0900

    modify link

--- src/component/Section4/Section4.js
+++ src/component/Section4/Section4.js
@@ -7,8 +7,8 @@ function Section4(props, section4Ref) {
             <ContactTitle>Link & Contact</ContactTitle>
             <LinksContainer>
                 <h3 style={{ color: '#f08080' }}>Links</h3>
-                <a href="https://github.com" target="_blank" rel="noreferrer">Github</a>
-                <a href="https://blog.example.com" target="_blank" rel="noreferrer">Blog</a>
+                <a href="https://github.com/kosy0907/" target="_blank" rel="noreferrer">Github</a>
+                <a href="https://kosy.tistory.com/" target="_blank" rel="noreferrer">Blog</a>
                 <h3 style={{ color: '#f08080' }}>Contact</h3>
                 <p>E-mail: seungyeonko98@gmail.com</p>
             </LinksContainer>
