451b0a9aebb2b6171192aad6f01dc6a4db0070c1
kosy0907 <syko1234@naver.com>
2024-11-26 17:41:16 +0900

    프로젝트 번호 수정

--- src/component/Section3/Tab/ProjectItem.js
+++ src/component/Section3/Tab/ProjectItem.js
@@ -1,6 +1,6 @@
 export const ProjectItem = [
     {
-        id: 2,
+        id: 1,
         title: 'Portfolio',
         period: '2024-09-13 ~ 2024-9-17',
         description: '프로젝트와 소개를 넣은 개인 포트폴리오 웹 사이트 입니다.',
@@ -24,7 +24,7 @@ export const ProjectItem = [
         color: '#03d2e0'
     },
     {
-        id: 3,
+        id: 2,
         title: 'EV-map',
         period: '2022-01-21 ~ 2022-01-30',
         description: '서울시 내의 지하철역 근처 교통약자용 엘리베이터 위치를 지도에 표시해주는 웹 사이트입니다.',
@@ -48,7 +48,7 @@ export const ProjectItem = [
         color: '#02422'
     },
     {
-        id: 4,
+        id: 3,
         title: '와이즈 워시',
         period: '2022-09 ~ 2022-12',
         description: '실시간 정보 공유를 중심으로 한 무인 세탁소 정보 공유 앱, "와이즈 워시"의 디자인 입니다.',
@@ -71,7 +71,7 @@ export const ProjectItem = [
         color: '#02422'
     },
     {
-        id: 5,
+        id: 4,
         title: 'Ucheck-Hour',
         period: '2022-09 ~ 2022-12',
         description: '주차장에서 플레이어의 자동차를 탈출시키는 게임, "RushHour"을 유니티로 구현했습니다. U check는 출석 체크 어플로, 지각 체크 전, 플레이어가 제 시간에 도착하는 것을 목표로 합니다.',
@@ -95,7 +95,7 @@ export const ProjectItem = [
         color: '#02422'
     },
     {
-        id: 6,
+        id: 5,
         title: 'OLook TestCase',
         period: '2021-03 ~ 2021-06',
         description: '팀 프로젝트로 진행한 패션 아이템 추천 사이트, OLook의 테스트 케이스입니다.',
@@ -119,7 +119,7 @@ export const ProjectItem = [
         code: '',
     },
     {
-        id: 7,
+        id: 6,
         title: 'EV-map TestCase',
         period: '2022-01-27 ~ 2022-01-29',
         description: 'EV-map 사이트의 테스트 케이스입니다.',
@@ -143,7 +143,7 @@ export const ProjectItem = [
         code: '',
     },
     {
-        id: 8,
+        id: 7,
         title: 'Portfolio TestCase',
         period: '2024-09-13 ~ 2024-09-17',
         description: '개인 포트폴리오 웹 사이트의 테스트 케이스입니다.',
